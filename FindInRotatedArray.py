c4007d376b034ca6bc7c3a24583151c34d0263d7
Sudheer Singampalli <singampallisudheer@gmail.com>
2022-07-11 22:29:22 +0530

    Update FindInRotatedArray.py

--- FindInRotatedArray.py
+++ FindInRotatedArray.py
@@ -1,5 +1,8 @@
 '''
 link : https://practice.geeksforgeeks.org/problems/search-in-a-rotated-array0959/1
+
+If a sorted array is rotated, then if we choose any element from the array, either side of the element will be sorted and other side will be unsorted.
+So, go to the sorted side and check if K could be there, if yes, reduce search space to left.. else right
 '''
 
 def Search(arr,n,k):
