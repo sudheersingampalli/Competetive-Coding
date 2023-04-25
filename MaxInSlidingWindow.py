08c2fd8ef0f9bfe21a9266512946205989cdecc0
Sudheer Singampalli <singampallisudheer@gmail.com>
2023-04-25 08:24:36 +0530

    Create MaxInSlidingWindow.py
    MaxInSlidingWindow

---
+++ MaxInSlidingWindow.py
@@ -0,0 +1,106 @@
+'''
+Problem Description
+Given an array of integers A. There is a sliding window of size B, moving from the very left of the array to the very right. You can only see the B numbers in the window. Each time the sliding window moves rightwards by one position. You have to find the maximum for each window.
+
+Return an array C, where C[i] is the maximum value in the array from A[i] to A[i+B-1].
+
+Refer to the given example for clarity.
+
+NOTE: If B > length of the array, return 1 element with the max of the array.
+
+
+
+Problem Constraints
+1 <= |A|, B <= 106
+
+
+
+Input Format
+The first argument given is the integer array A.
+
+The second argument given is the integer B.
+
+
+
+Output Format
+Return an array C, where C[i] is the maximum value of from A[i] to A[i+B-1].
+
+
+
+Example Input
+Input 1:
+
+ A = [1, 3, -1, -3, 5, 3, 6, 7]
+ B = 3
+Input 2:
+
+ A = [1, 2, 3, 4, 2, 7, 1, 3, 6]
+ B = 6
+
+
+Example Output
+Output 1:
+
+ [3, 3, 5, 5, 6, 7]
+Output 2:
+
+ [7, 7, 7, 7]
+
+
+Example Explanation
+Explanation 1:
+
+ Window position     | Max
+ --------------------|-------
+ [1 3 -1] -3 5 3 6 7 | 3
+ 1 [3 -1 -3] 5 3 6 7 | 3
+ 1 3 [-1 -3 5] 3 6 7 | 5
+ 1 3 -1 [-3 5 3] 6 7 | 5
+ 1 3 -1 -3 [5 3 6] 7 | 6
+ 1 3 -1 -3 5 [3 6 7] | 7
+Explanation 2:
+
+ Window position     | Max
+ --------------------|-------
+ [1 2 3 4 2 7] 1 3 6 | 7
+ 1 [2 3 4 2 7 1] 3 6 | 7
+ 1 2 [3 4 2 7 1 3] 6 | 7
+ 1 2 3 [4 2 7 1 3 6] | 7
+'''
+
+from collections import deque
+class Solution:
+	# @param A : tuple of integers
+	# @param B : integer
+	# @return a list of integers
+	def slidingMaximum(self, A, B):
+		l = deque()
+		res = []
+		# do for the first B items
+		for i in range(B):
+			while l and A[i] >= A[l[-1]]:
+				l.pop()
+			l.append(i)
+		
+		for i in range(B, len(A)):
+
+			# append the left most ele of deque to res
+			res.append(A[l[0]])
+
+			# remove the eles which are out of this window
+			while(l and l[0]<= i-B):
+				l.popleft()
+			
+			# remove useless ele
+			while l and A[i]>=A[l[-1]]:
+				l.pop()
+			l.append(i)
+		
+		res.append(A[l[0]])
+		return res
+		
+		
+s = Solution()
+A = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+B = 2
+print(s.slidingMaximum(A,B))
