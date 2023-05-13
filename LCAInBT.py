2c8169d3865d5f638d7cb1d47ae611b58eab2704
Sudheer Singampalli <singampallisudheer@gmail.com>
2023-05-13 22:48:26 +0530

    Update LCAInBT.py

--- LCAInBT.py
+++ LCAInBT.py
@@ -16,29 +16,29 @@ class Solution:
 	# @param C : integer
 	# @return an integer
 	def lca(self, A, B, C):
-        allnodes = []
-      # inorder traversal to make a note of all the nodes to check later if B and C exists
-        def inorder(A):
-            if A:
-                inorder(A.left)
-                allnodes.append(A.val)
-                inorder(A.right)
+		allnodes = []
+	      # inorder traversal to make a note of all the nodes to check later if B and C exists
+		def inorder(A):
+		    if A:
+			inorder(A.left)
+			allnodes.append(A.val)
+			inorder(A.right)
 
-        def lcahelper(A, B, C):
-            if A is None: return None
-            if A.val == B or A.val == C: return A
+		def lcahelper(A, B, C):
+		    if A is None: return None
+		    if A.val == B or A.val == C: return A
 
-            L = lcahelper(A.left, B, C)
-            R = lcahelper(A.right, B, C)
+		    L = lcahelper(A.left, B, C)
+		    R = lcahelper(A.right, B, C)
 
-            if L and R: return A
-            if L: return L
-            if R: return R
-            else : return None
+		    if L and R: return A
+		    if L: return L
+		    if R: return R
+		    else : return None
         
-        arr = inorder(A)
-        if B not in allnodes or C not in allnodes:
-            return -1
+	arr = inorder(A)
+	if B not in allnodes or C not in allnodes:
+	    return -1
         res = lcahelper(A, B, C)
         # print("res", res.val)
         return res.val if res else None
